f8e6cf767016798ae92ad972343bb2840e832ca1
JJ-hw <110426712+JJ-hw@users.noreply.github.com>
2025-09-15 13:55:11 +0200

    [FEATURE][SYN] Improve timing : use different strategy (ExtraTimingOpt); move phys_opt_design in route.pre hook, so that it is done after the default one; remove unused directive in route.pre hook.

--- versal/constraints/hooks/hook_place.post.tcl
+++ versal/constraints/hooks/hook_place.post.tcl
@@ -1,11 +1,4 @@
 # BSD 3-Clause Clear License
 # Copyright © 2025 ZAMA. All rights reserved.
 
-report_timing_summary -rpx place_timing_summary.rpx -max_paths 1000 -file place_timing_summary.rpt
 
-set_clock_uncertainty -setup 0.2 [get_clocks *]
-phys_opt_design -directive AggressiveExplore
-phys_opt_design -directive AggressiveExplore
-set_clock_uncertainty -setup 0 [get_clocks *]
-
-report_timing_summary -rpx place_timing_summary.rpx -max_paths 1000 -file physopt2_timing_summary.rpt
